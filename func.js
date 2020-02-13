67cf3c25ca1479308deaaf35593015f0856f9fcb
IPSOSGROUP\ndikov01 <nikolay.dikov@ipsos.com>
2020-02-13 18:03:20 +0200

    It's working :)

--- func.js
+++ func.js
@@ -45,7 +45,14 @@ $(document).ready(function(){
         "border-radius":"3px",
         "cursor": "pointer"
     });
+
     $('.Button').height((($('#Container').height()-$('#DisplayBox').height())/5)-2+"px");
+    $('.Button').width(($('#Container').width()/4)-2+"px");
+    $( window ).resize(function() {
+        $('.Button').height((($('#Container').height()-$('#DisplayBox').height())/5)-2+"px");
+        $('.Button').width(($('#Container').width()/4)-2+"px");
+    });
+    
     $('.innerText').css({
         "position":"absolute",
         "bottom":"0",
@@ -59,34 +66,86 @@ $(document).ready(function(){
         "justify-content": "center", /* align horizontal */
         "align-items": "center",
     });
-    $('#Button\\.0\\.0 span').html("%");
-    $('#Button\\.0\\.1 span').html("CE");
-    $('#Button\\.0\\.2 span').html("C");
-    $('#Button\\.0\\.3 span').html("/");
+    $('#Button\\.0\\.0 span').html("%").addClass("ActiveB Operator");
+    $('#Button\\.0\\.1 span').html("CE").addClass("ClearLast");
+    $('#Button\\.0\\.2 span').html("C").addClass("ClearAll");
+    $('#Button\\.0\\.3 span').html("/").addClass("ActiveB Operator");
 
-    $('#Button\\.1\\.0 span').html("7");
-    $('#Button\\.1\\.1 span').html("8");
-    $('#Button\\.1\\.2 span').html("9");
-    $('#Button\\.1\\.3 span').html("*");
+    $('#Button\\.1\\.0 span').html("7").addClass("ActiveB");
+    $('#Button\\.1\\.1 span').html("8").addClass("ActiveB");
+    $('#Button\\.1\\.2 span').html("9").addClass("ActiveB");
+    $('#Button\\.1\\.3 span').html("*").addClass("ActiveB Operator");
 
-    $('#Button\\.2\\.0 span').html("4");
-    $('#Button\\.2\\.1 span').html("5");
-    $('#Button\\.2\\.2 span').html("6");
-    $('#Button\\.2\\.3 span').html("-");
+    $('#Button\\.2\\.0 span').html("4").addClass("ActiveB");
+    $('#Button\\.2\\.1 span').html("5").addClass("ActiveB");
+    $('#Button\\.2\\.2 span').html("6").addClass("ActiveB");
+    $('#Button\\.2\\.3 span').html("-").addClass("ActiveB Operator");
     
-    $('#Button\\.3\\.0 span').html("1");
-    $('#Button\\.3\\.1 span').html("2");
-    $('#Button\\.3\\.2 span').html("3");
-    $('#Button\\.3\\.3 span').html("+");
+    $('#Button\\.3\\.0 span').html("1").addClass("ActiveB");
+    $('#Button\\.3\\.1 span').html("2").addClass("ActiveB");
+    $('#Button\\.3\\.2 span').html("3").addClass("ActiveB");
+    $('#Button\\.3\\.3 span').html("+").addClass("ActiveB Operator");
 
-    $('#Button\\.4\\.0 span').html(".");
-    $('#Button\\.4\\.1 span').html("0");
-    $('#Button\\.4\\.2 span').html(".");
-    $('#Button\\.4\\.3 span').html("=");
+    $('#Button\\.4\\.0 span').html(".").addClass("ActiveB");
+    $('#Button\\.4\\.1 span').html("0").addClass("ActiveB");
+    $('#Button\\.4\\.2 span').html(".").addClass("ActiveB");
+    $('#Button\\.4\\.3 span').html("=").addClass("EndIt");
 
-    
-    $('.Button').click(function(){
-        $('#innerBox').html($('#innerBox').html()+$(this).find("span").html());
+    var expression1="";
+    var expression2="";
+    var operator1="";
+    var temp=1;
+
+    $('.ActiveB').click(function(){
+        
+        if ($(this).hasClass("Operator")) {
+            if (expression1=="") {
+                expression1=$('#innerBox').html();
+            }
+            operator1=$(this).html();
+            temp=2
+            $('#innerBox').html($('#innerBox').html()+$(this).html());
+            console.log(operator1);
+        }else{
+            if (temp==1) {
+                if (expression1=="") {
+                    $('#innerBox').html("");
+                }
+                if (expression1.indexOf(".")!=-1 && $(this).html() == ".") {
+                }else{
+                    expression1+=$(this).html();
+                    $('#innerBox').html($('#innerBox').html()+$(this).html());
+                }
+                console.log(expression1);
+            }else{
+                if (expression2.indexOf(".")!=-1 && $(this).html() == ".") {
+                }else{
+                    expression2+=$(this).html();
+                    $('#innerBox').html($('#innerBox').html()+$(this).html());
+                }
+                console.log(expression2);
+            }
+        }
+        
+    })
+    $('.ClearAll').click(function(){
+        $('#innerBox').html("");
+        expression1="";
+        expression2="";
+        operator1="";
+        temp=1;
+    })
+    $('.ClearLast').click(function(){
+        $('#innerBox').html($('#innerBox').html().slice(0,-1));
+    })
+    $('.EndIt').click(function(){
+        if (expression1!='' && expression2!='') {
+            $('#innerBox').html(eval(expression1+operator1+expression2));
+            expression1="";
+            expression2="";
+            operator1="";
+            temp=1;
+        }
     })
 
 
